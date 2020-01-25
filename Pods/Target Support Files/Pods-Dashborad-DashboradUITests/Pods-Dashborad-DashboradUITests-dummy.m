7d5da40fbda79b61474a691cb34944c148535c0c
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 11:04:57 +0800

    add cocoaPod

---
+++ Pods/Target Support Files/Pods-Dashborad-DashboradUITests/Pods-Dashborad-DashboradUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dashborad_DashboradUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dashborad_DashboradUITests
+@end
---
+++ Pods/Target Support Files/Pods-DashboradTests/Pods-DashboradTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DashboradTests : NSObject
+@end
+@implementation PodsDummy_Pods_DashboradTests
+@end
