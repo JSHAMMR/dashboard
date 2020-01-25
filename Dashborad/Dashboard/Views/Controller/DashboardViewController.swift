5eb7138b706c268e035a3ff715c081daffe3ab15
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 15:45:04 +0800

    add func changeScope and delegateScope to DashboardViewController

--- Dashborad/Dashboard/Views/Controller/DashboardViewController.swift
+++ Dashborad/Dashboard/Views/Controller/DashboardViewController.swift
@@ -16,6 +16,12 @@ class DashboardViewController: UIViewController {
     @IBOutlet var jobProvider: JobsProvider!
     @IBOutlet var servicesProvider: ServicesProvider!
 
+    @IBOutlet weak var scopeLbl: UILabel!
+    fileprivate var scopes = ["ALL","TODAY","LAST_7_DAYS","LAST_30_DAYS"]
+    fileprivate var scopesPickerView = UIPickerView()
+    fileprivate var scopesValue = String()
+    
+    
     override func viewDidLoad() {
         
         self.dashboardListPresenter = self.dashboardPresenter()
@@ -41,5 +47,46 @@ class DashboardViewController: UIViewController {
            
            return dashboardListPresenter
     }
+    
+    @IBAction func changeScope(_ sender: Any) {
+       
+       let alert = UIAlertController(title: "Dashboard filter", message: "Dashboard Data will filter based on this \n\n\n\n\n\n", preferredStyle: .actionSheet)
+       alert.isModalInPopover = true
+       
+        let pickerFrame = UIPickerView(frame: CGRect(x: 0, y: 0, width: alert.view.frame.width, height: 200 ))
+       
+       alert.view.addSubview(pickerFrame)
+       pickerFrame.dataSource = self
+       pickerFrame.delegate = self
+       
+       alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+       alert.addAction(UIAlertAction(title: "Apply", style: .default, handler: { (UIAlertAction) in
+           
+        
+        self.dashboardListPresenter?.fetchDashboardItems(scope: self.scopesValue )
+
+        self.scopeLbl.text = self.scopesValue
+       
+       }))
+       self.present(alert,animated: true, completion: nil )
+        
+    }
 }
 
+extension DashboardViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return scopes.count
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return scopes[row]
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+       scopesValue = scopes[row]
+    }
+}
