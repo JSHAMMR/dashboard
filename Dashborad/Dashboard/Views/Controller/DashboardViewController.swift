8403159d7a5867b2c9bd84fc7d0b071f0eacb54b
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 11:59:16 +0800

    add scrollview to DashboardviewController

--- Dashborad/Dashboard/Views/Controller/DashboardViewController.swift
+++ Dashborad/Dashboard/Views/Controller/DashboardViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
