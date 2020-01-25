43419a95575a145ac56d4d4cb51edcf45a562be1
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 11:43:24 +0800

    add DashboardViewModel

--- Dashborad/Dashboard/ViewModel/DashboardViewModel.swift
+++ Dashborad/Dashboard/ViewModel/DashboardViewModel.swift
@@ -6,8 +6,40 @@
 //  Copyright © 2020 Jamil. All rights reserved.
 //
 
+
 import UIKit
 
-class DashboardViewModel: NSObject {
+protocol DashboardListModelDelegate : AnyObject {
+    func didFetchDashboardItems(success:Bool, dashboardItems:DashboardModel)
+}
 
+open class DashboardViewModel {
+    weak var delegate: DashboardListModelDelegate?
+    private let networkLayer: Network
+
+    init(networkLayer:Network) {
+        self.networkLayer = networkLayer
+    }
+    
+    open func fetchDashboardItems(scope:String = "All") {
+        self.networkLayer.executeGETRequest(api: scope, completionBlock: { (data) in
+           
+            
+            var dashboardModel:DashboardModel?
+            
+            if let dashboardItemsData = data {
+                let dashboardParser = DashboardParser()
+                dashboardModel = dashboardParser.parseDashboardItems(data: dashboardItemsData)
+                if let delegate = self.delegate {
+                    delegate.didFetchDashboardItems(success: true, dashboardItems: dashboardModel!)
+                    return
+                }
+            }
+            
+            if let delegate = self.delegate {
+                delegate.didFetchDashboardItems(success: false, dashboardItems: dashboardModel!)
+                return
+            }
+        })
+    }
 }
