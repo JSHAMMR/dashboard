d79721d42d8f175a4ec9c8ba49aa0091ce0d5d1e
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 11:52:21 +0800

    add DashboardListPresenter

---
+++ Dashborad/Dashboard/Presenter/DashboardListPresenter.swift
@@ -0,0 +1,39 @@
+//
+//  DashboardListPresenter.swift
+//  Dashborad
+//
+//  Created by Gamil Ali Qaid Shamar on 25/01/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import UIKit
+
+protocol DashboardListPresenterDelegate : AnyObject {
+    func didFetchDashboardItems(success:Bool)
+}
+
+class DashboardListPresenter: NSObject {
+    weak var delegate: DashboardListPresenterDelegate?
+    private let dashboardViewModel: DashboardViewModel
+    private var dashboardModel: DashboardModel!
+    
+    init(dashboardViewModel:DashboardViewModel) {
+           self.dashboardViewModel = dashboardViewModel
+           super.init()
+           self.dashboardViewModel.delegate = self
+       }
+
+}
+
+extension DashboardListPresenter : DashboardListModelDelegate {
+   
+    func didFetchDashboardItems(success: Bool, dashboardItems: DashboardModel) {
+        self.dashboardModel = dashboardItems
+        
+        if let delegate = self.delegate {
+            delegate.didFetchDashboardItems(success: success)
+            return
+        }
+    }
+    
+}
