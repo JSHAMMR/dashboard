fa41c8a4f5d80ec486393a927ae850beae499817
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 13:52:52 +0800

    setup LineChartView

---
+++ Dashborad/Dashboard/Views/XIBs/LineChartView.swift
@@ -0,0 +1,21 @@
+//
+//  LineChartView.swift
+//  Dashborad
+//
+//  Created by Gamil Ali Qaid Shamar on 25/01/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class LineChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
