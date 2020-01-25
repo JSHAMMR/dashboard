a5d62e051ff746ff07bf143fb81e43ef6c3d79eb
Jamil Shamar <jameelshammr@gmail.com>
2020-01-25 13:22:34 +0800

    add JobsView

---
+++ Dashborad/Dashboard/Views/XIBs/JobsView.swift
@@ -0,0 +1,24 @@
+//
+//  JobsView.swift
+//  Dashborad
+//
+//  Created by Gamil Ali Qaid Shamar on 25/01/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class JobsView: UIView {
+
+    
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+    class func instanceFromNib() -> UIView {
+        return UINib(nibName: "JobsView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
+    }
+}
